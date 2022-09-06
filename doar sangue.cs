dac0b04cb1d5d8df5a6304f1c313bf228f4120b1
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-06 16:53:06 -0300

    prog. idade pra doar sangue

---
+++ doar sangue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace idade_doar_sangue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            Console.Write("sua Idade:");
+                idade = int.Parse(Console.ReadLine());
+            if (idade >=18 && idade <= 67)
+            {
+                Console.WriteLine("pode doar sangue");
+            }
+            else
+            {
+                Console.WriteLine("nao pode doar sangue");
+            }
+
+
+
+        }
+    }
+}
