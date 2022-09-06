cbd44bce4251d473d2d3f09f1bb6b1e90ff1015f
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-06 20:04:32 -0300

    Add files via upload

---
+++ % de aumento do salario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, porcentagem, novo_salario;
+            Console.WriteLine("Digite o salario: ");
+            salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o percentual: ");
+            porcentagem = double.Parse(Console.ReadLine());
+
+            novo_salario = salario * porcentagem / 100 + salario;
+
+            Console.WriteLine("Novo salario " + novo_salario);
+        }
+    }
+}
