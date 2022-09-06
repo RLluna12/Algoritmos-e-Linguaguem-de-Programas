969ed15bf38d2a66d23073e4d556cbe7187d7ed1
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-06 15:57:06 -0300

    mult de 7

---
+++ mult de 7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplo_de_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+                Console.Write("Digite seu numero inteiro: ");
+            n1 = int.Parse(Console.ReadLine());
+            
+            if (n1 %7==0)
+            {
+                Console.WriteLine("seu numero é um multiplo de 7");
+            }
+            else
+            {
+                Console.WriteLine("nao é um multiplo de 7");
+            }
+
+        }
+    }
+}
