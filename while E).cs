935bbfab6cc4bbd7546c3f38d18b8ebdb9c91c8d
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-19 11:17:50 -0300

    Exercicios while 1-e)

---
+++ while E).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int i = 1, soma = 0;
+            while (i <= 50)
+            {
+                i++;
+                if (i % 2 == 0)
+                {
+                    soma = soma + i;
+                    Console.WriteLine("Resultado: " + soma);
+                }
+            }
+
+            
+            
+
+            
+
+        }
+    }
+}
