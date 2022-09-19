c0a4515b04f1591ff8c4d6caeb5102f1f8f48e41
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-19 11:12:12 -0300

    Exercicios 1-b)

---
+++ while B).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, num=0 ;
+            while (i<=100)
+            {
+             num = num + i; 
+             i++;
+  
+             Console.WriteLine(num);
+            }
+          
+            
+            Console.WriteLine("Resultado: "+num);           
+            
+
+         
+            
+
+
+        }
+    }
+}
