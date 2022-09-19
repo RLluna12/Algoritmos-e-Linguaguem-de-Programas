1d144e7dea8a585ab0eac3ea64a9ed73dbcc3bc5
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-19 11:12:36 -0300

    Exercicios while 1-c)

---
+++ while C).cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 30)
+            {
+             if(i % 4 == 0)
+                    {
+                    Console.WriteLine(i);
+                }
+                    i++;
+            }
+
+        }
+    }
+}
