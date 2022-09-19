328a2cb871b777d41c71db33def53e78f4ade4ff
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-19 11:13:14 -0300

    Exercicios while 1-d)

---
+++ while D).cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 15, q = 0;
+            while (i <= 200)
+            {
+                q = i * i;
+                Console.WriteLine(q);
+                i++;
+                
+            }
+           
+
+        }
+    }
+}
