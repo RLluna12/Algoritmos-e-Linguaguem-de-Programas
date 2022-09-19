e900ec6c6de3a5bb82634ad11bf0ef8cec081bcc
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-19 11:11:35 -0300

    Exercicios while 1-a)

---
+++ while 1-A).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 1;
+            //while (i<20)
+            //{
+            //  if (i % 2 == 1)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            int i;
+            for (i = 1; i < 20; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
