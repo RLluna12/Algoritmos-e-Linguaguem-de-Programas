b6d2ef591a2c37163cd86531d4de1a50ff321d87
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-26 10:48:04 -0300

    2-10 ate 1 e mostrar invertido.

---
+++ 2-10 ate 1 e mostrar invertido.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[10];
+            v[0] = 10;
+            v[1] = 9;
+            v[2] = 8;
+            v[3] = 7;
+            v[4] = 6;
+            v[5] = 5;
+            v[6] = 4;
+            v[7] = 3;
+            v[8] = 2;
+            v[9] = 1;
+
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(v[i]);
+            }
+        }
+    }
+}
