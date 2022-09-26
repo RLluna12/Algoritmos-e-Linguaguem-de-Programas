d325e9057270627eac5edb7db77d73308db62d42
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-09-26 10:30:52 -0300

    1-10 numeros aparece impares.

---
+++ 1-10 numeros aparece impares.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int []v= new int[10], impar;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite numero: ");
+                v[i] = int.Parse(Console.ReadLine());    
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                
+                if (v[i] % 2 == 1)
+                {
+                   
+                    Console.WriteLine("Numeros Impares: "+v[i]);
+                }
+
+
+
+
+            }
+        }
+    }
+}
