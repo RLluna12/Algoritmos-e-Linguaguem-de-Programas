8ae44e036e703ee7860af61bc0b1bf8a170c4891
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-03 11:34:55 -0300

    Metodo 2-area do triangulo METODO.

---
+++ 2-area do triangulo METODO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double area, Base;
+            Console.WriteLine("Digite area do triangulo:");
+            area = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite base do triangulo: ");
+            Base = double.Parse(Console.ReadLine());
+            double mult = M(area, Base);
+            Console.WriteLine(mult);
+        }
+        public static double M(double x, double y)
+        {
+            double mult = (x * y)/2;
+            return mult;
+        }
+    }
+}
