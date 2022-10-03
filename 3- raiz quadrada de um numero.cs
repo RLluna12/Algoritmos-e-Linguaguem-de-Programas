3e870c782ccc4721a78c8bfc25c19c703a7608f9
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-03 11:35:14 -0300

    Metodo 3- raiz quadrada de um numero.

---
+++ 3- raiz quadrada de um numero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double a, r;
+            Console.WriteLine("Digite um numero:");
+            a = double.Parse(Console.ReadLine());
+            r = M(a);
+            Console.WriteLine(r);
+        }
+        public static double M(double x)
+        {
+            double r = 0;
+            r+=Math.Sqrt(x);
+            return r;
+        }
+    }
+}
