3bf21b93fc2a4ccfbba7b71387c1af72fe782c41
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-03 11:34:38 -0300

    Metodo 1- mult de 2 numeros.

---
+++ 1- mult de 2 numeros.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            double a, b;
+            Console.WriteLine("Digite numero a:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um numero: ");
+            b = double.Parse(Console.ReadLine());
+            double mult = M(a, b);
+            Console.WriteLine(mult);
+        }
+        public static double M (double x,double y)
+        {
+            double mult = x * y;
+            return mult;
+        }
+    }
+}
