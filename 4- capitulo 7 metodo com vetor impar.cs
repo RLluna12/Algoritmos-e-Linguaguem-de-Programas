6d9d68b8544f334abe03e766331163e3d9013656
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-04 00:28:16 -0300

    4- capitulo 7 metodo com vetor impar.cs

---
+++ 4- capitulo 7 metodo com vetor impar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp39
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] v = new int[10];
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+
+            }
+            M(ref v);
+
+
+        }
+        public static void M(ref int[] x)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (x[i] % 2 == 1)
+                {
+                    Console.WriteLine(x[i]);
+                }
+               
+
+            }
+        }
+    }
+}
