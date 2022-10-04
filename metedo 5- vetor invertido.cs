a0542e845aa55b8d86d6335937904d5ed2e02870
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-04 00:28:40 -0300

    metedo 5- vetor invertido.cs

---
+++ metedo 5- vetor invertido.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp38
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] v = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+              M(ref v);
+        }
+        public static void M(ref int[] x)
+        {
+
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(x[i]);
+
+            }
+        }
+    }
+}
