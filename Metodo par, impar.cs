d265e00e2860c4782728deb9e81fdd39809ed7c1
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-31 10:33:03 -0300

    Metodo par, impar

---
+++ Metodo par, impar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            int v, a;
+            a = int.Parse(Console.ReadLine());
+            v = M(a);
+            Console.WriteLine(v);
+        }
+        public static int M(int a)
+       {
+            
+            if (a % 2 == 0)
+            {
+                return a = 0;
+            }
+            else
+            {
+                return a=1;
+            }
+       }
+    }
+}
