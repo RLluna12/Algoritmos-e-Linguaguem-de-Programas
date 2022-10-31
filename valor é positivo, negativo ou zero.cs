c6ca81c524cc985d67557fcd26416c5762dbed95
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-31 10:38:40 -0300

    valor é positivo, negativo ou zero

---
+++ valor é positivo, negativo ou zero.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int v, a;
+            a = int.Parse(Console.ReadLine());
+            v = M(a);
+            Console.WriteLine(v);
+        }
+        public static int M(int a)
+        {
+            //numero 0
+            if (a == 0)
+            {
+                return a = 0;
+            }
+            //numero positivo
+            if (a>0)
+            {
+                return a = 1;
+            }
+            //numero negativo
+            else
+            {
+                return a = -1;
+            }
+        }
+    }
+}
